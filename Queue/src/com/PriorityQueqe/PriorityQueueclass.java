98547ed67ec101299f96168650fa31de2a72211a
sanjeet <skkarmasi420@gmail.com>
2025-12-10 17:55:03 +0530

    Priority Queue Kth largest ELement

---
+++ Queue/src/com/PriorityQueqe/PriorityQueueclass.java
@@ -0,0 +1,11 @@
+package com.PriorityQueqe;
+
+import java.util.Queue;
+
+public class PriorityQueue {
+
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq  = new PriorityQueue() ;
+
+    }
+}
