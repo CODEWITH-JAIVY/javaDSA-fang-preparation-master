589f27ea2553524c9fa6514f687b2a321c983b5d
sanjeet <skkarmasi420@gmail.com>
2026-03-06 09:59:57 +0530

    06/03/2026 leetcode solved problem

---
+++ untitled/src/MinimunChange1758.java
@@ -0,0 +1,6 @@
+public class MinimunChange {
+    public int minOperations(String s) {
+         int count = 0 ;
+         char []  charArray  = s.toCharArray() ;
+    }
+    }
---
+++ untitled/src/segment1784.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class segment1784 {
+}
